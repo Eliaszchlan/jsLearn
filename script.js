8686ee62965ab0d3250036d68335d70d13a132a4
Eliasz <eliaszchlan@wp.pl>
2022-07-25 15:20:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let incr = 10,
+    decr = 10;
+
+
+//++incr;
+//--decr;
+
+//console.log(incr++);
+//console.log(decr--);
+
+//console.log(2 * 4 == '8');
+//console.log(2 * 4 === '8');
+
+
+//const isChecked = true,
+//islose = false;
+
+//console.log(isChecked || islose);
+
+//console.log((2 + 2) * 2 !== '8');
